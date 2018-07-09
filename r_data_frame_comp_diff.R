217f2b11f236fa7f191d17aa7ca6227c12ca52d9
Yifan_GK <yifanx@uchicago.edu>
2018-07-09 18:26:58 -0500

    update output formats of functions

--- r_data_frame_comp_diff.R
+++ r_data_frame_comp_diff.R
@@ -17,9 +17,9 @@ df_comp = function(df_1, df_2, i = 1, j = nrow(df_1)) {
   df_1 = as.data.table(df_1)
   df_2 = as.data.table(df_2)
   if (all.equal(df_1[i:j], df_2[i:j]) == TRUE) {
-    print("the two data frames are identical")
+    message("the two data frames are identical")
   } else {
-    print("the two data frames are different")
+    message("the two data frames are different")
   }
 }
 
@@ -27,16 +27,16 @@ df_comp = function(df_1, df_2, i = 1, j = nrow(df_1)) {
 
 # 2. pin down the different rows
 df_diff = function(df_1, df_2, i = 1, j = nrow(df_1)) {
-  library(data.table)
+  suppressMessages(library(data.table))
   df_1 = as.data.table(df_1)
   df_2 = as.data.table(df_2)
   # first decide whether the function is doable
   # i.e., want to check whether df_1 and df_2 both have enough rows to compare
   if (min(nrow(df_1), nrow(df_2)) < max(i, j)) {
-    print("At least one of the two data frames does not have enough rows")
+    warning("At least one of the two data frames does not have enough rows", call. = FALSE)
   } else if (all.equal(df_1[i:j], df_2[i:j]) == TRUE) {
     # this is for if identical
-    print("All good! The rows checked are all identical :)")
+    message("All good! The rows checked are all identical :)")
     
   } else {
     # now that we know they are different, start finding the difference
@@ -45,14 +45,14 @@ df_diff = function(df_1, df_2, i = 1, j = nrow(df_1)) {
     for (k in i:j) {
       if (all.equal(df_1[k], df_2[k]) != TRUE) {
         # print a message when each bad row is found
-        cat(paste0("row ", k, " is different, with the problem below:", "\n",
-                   all.equal(df_1[k], df_2[k]), "\n\n"))
+        cat(paste0("row ", k, " is different, with the problem:", "\n",
+                   all.equal(df_1[k], df_2[k]), "\n"))
         # update the bad rows list w/ the bad rows found
         bad_rows = c(bad_rows, k)
       }
     }
     # print a message with indices of all the bad rows
-    cat("there are in total ", length(bad_rows),
+    message("there are in total ", length(bad_rows),
         " row(s) that differ in (selected part of) the two data frames, ",
         "and below are their indices:", "\n", toString(bad_rows), sep = "")
     }
